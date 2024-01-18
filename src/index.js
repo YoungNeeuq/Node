4477bba520adc8529c31a90a9c5fa9ac6f131e9d
YoungNeeuq <haulv.dev@gmail.com>
2024-01-19 02:41:44 +0700

    Update code expressJs test

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars')
+const port = 3000
+const router = require('./routers')
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded());
+app.use(express.json())
+//template engine
+app.engine('handlebars', handlebars.engine());
+app.set('view engine','handlebars')
+
+
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log(path.join(__dirname, 'resources/views'))
+
+
+router(app)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
